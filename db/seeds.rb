dd2c69bded6e69172b199562f96726dbda69bbcc
Christopher Kelling <cj.kelling88@gmail.com>
2020-02-17 09:15:02 -0700

    Seed database with 4 Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  name: 'Christopher',
+  email: 'ck@email.com',
+  password: 'password',
+  city: 'Northglenn',
+  state: 'CO',
+  meetup_radius: '25',
+  field_of_interest: 'Software Development',
+  about_me: 'I want to learn more about software development.',
+  gender: 'He/Him',
+  image: 'https://ca.slack-edge.com/T029P2S9M-UK4P0D42W-08ba1d31a7f6-72'
+)
+User.create!(
+  name: 'Mary',
+  email: 'ml@email.com',
+  password: 'password',
+  city: 'Boulder',
+  state: 'CO',
+  meetup_radius: '20',
+  field_of_interest: 'Software Development',
+  about_me: 'I want to learn more about software development.',
+  gender: 'She/Her',
+  image: 'https://ca.slack-edge.com/T029P2S9M-UKFAC39T8-a2bcff612d11-72'
+)
+User.create!(
+  name: 'Kayla',
+  email: 'kw@email.com',
+  password: 'password',
+  city: 'Denver',
+  state: 'CO',
+  meetup_radius: '15',
+  field_of_interest: 'Software Development',
+  about_me: 'I want to learn more about software development.',
+  gender: 'She/Her',
+  image: 'https://ca.slack-edge.com/T029P2S9M-UKDBXCVSR-c9dfc59451e1-72'
+)
+User.create!(
+  name: 'Ben',
+  email: 'bf@email.com',
+  password: 'password',
+  city: 'Golden',
+  state: 'CO',
+  meetup_radius: '10',
+  field_of_interest: 'Software Development',
+  about_me: 'I want to learn more about software development.',
+  gender: 'He/Him',
+  image: 'https://ca.slack-edge.com/T029P2S9M-UK28AK7FD-4caa1e486b61-72'
+)
