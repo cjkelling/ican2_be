83a0eb9f81f6a2580889fe747f160c799fed3376
Mary Lang <mcl86@cornell.edu>
2020-02-18 12:16:40 -0700

    Add user type to graphql types

---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,26 @@
+module Types
+
+  class UserType < Types::BaseObject
+    field :id, Int, null: false
+    field :name, String, null: false
+    field :email, String, null: false
+    field :password_digest, String, null: false
+    field :city, String, null: false
+    field :state, String, null: false
+    field :meetup_radius, Int, null: false
+    field :field_of_interest, String, null: false
+    field :about_me, String, null: false
+    field :gender, String, null: false
+    field :image, String, null: false
+    field :mentor, Boolean, null: false
+
+
+    field :field_of_knowledge, String, null: true
+    field :experience_level, String, null: true
+    field :work_day_question, String, null: true
+    field :enjoyment_question, String, null: true
+    field :teaching_points_question, String, null: true
+    field :advice_question, String, null: true
+  end
+
+end
