a16d47d29e71e1e4b3ec1b27c771dfbd88ce046b
Mary Lang <mcl86@cornell.edu>
2020-02-18 15:20:00 -0700

    WIP testing query with argument

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,14 +2,14 @@ module Types
   class QueryType < Types::BaseObject
 
     field :mentors, [Types::UserType], null: false  do
-      argument :location, String, required: false
+      argument :zip_code, String, required: false
       argument :gender, String, required: false
       argument :field, String, required: false
       argument :expert_lvl, String, required: false
     end
 
-    def mentors(location: nil, gender: nil, field: nil, expert_lvl: nil)
-      User.get_all_mentors({location: location, gender: gender, field: field, expert_lvl: expert_lvl})
+    def mentors(zip_code: nil, gender: nil, field: nil, expert_lvl: nil)
+      User.get_all_mentors({zip_code: zip_code, gender: gender, field: field, expert_lvl: expert_lvl})
     end
 
     field :users, Types::UserType, null: false do
--- spec/requests/get_user_spec.rb
+++ spec/requests/get_user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'get user endpoint' do
   before(:each) do
     @mentor = User.create!(id: 1, name: 'Mary', email: 'ml@email.com',  password: 'password',
-      city: 'Boulder',  state: 'CO',  meetup_radius: '20',  field_of_interest: 'Software Development',
+      city: 'Boulder',  state: 'CO', zip_code: '80202',  meetup_radius: '20',  field_of_interest: 'Software Development',
       about_me: 'I want to learn more about software development.',  gender: 'She/Her',
       image: 'https://ca.slack-edge.com/T029P2S9M-UKFAC39T8-a2bcff612d11-72',  mentor: true,
       field_of_knowledge: 'Software Development',  experience_level: 'Advanced',
@@ -11,7 +11,7 @@ describe 'get user endpoint' do
       teaching_points_question: 'I want to teach Ruby on Rails!', advice_question: 'Just keep swimming!' )
 
     @mentor2 = User.create!(id: 2, name: 'Kayla', email: 'kw@email.com', password: 'password',
-      city: 'Denver', state: 'CO', meetup_radius: '15', field_of_interest: 'Software Development',
+      city: 'Denver', state: 'CO', zip_code: '90210', meetup_radius: '15', field_of_interest: 'Software Development',
       about_me: 'I want to learn more about software development.', gender: 'She/Her',
       image: 'https://ca.slack-edge.com/T029P2S9M-UKDBXCVSR-c9dfc59451e1-72', mentor: true,
       field_of_knowledge: 'Software Development', experience_level: 'Intermediate',
@@ -19,14 +19,44 @@ describe 'get user endpoint' do
       teaching_points_question: 'I want to teach React Native!', advice_question: 'Just keep swimming!' )
 
     @mentor3 = User.create!(id: 3, name: 'Ben', email: 'bf@email.com', password: 'password',
-      city: 'Golden', state: 'CO', meetup_radius: '10', field_of_interest: 'Software Development',
+      city: 'Golden', state: 'CO', zip_code: '90302', meetup_radius: '10', field_of_interest: 'Software Development',
       about_me: 'I want to learn more about software development.',
       gender: 'He/Him', image: 'https://ca.slack-edge.com/T029P2S9M-UK28AK7FD-4caa1e486b61-72',
       mentor: true, field_of_knowledge: 'Software Development', experience_level: 'Beginner',
       work_day_question: 'My typical day is awesome!', enjoyment_question: 'I enjoy teaching and sharing my knowledge!',
       teaching_points_question: 'I want to teach Javascript!', advice_question: 'Just keep swimming!' )
   end
-  it 'sends a user object' do
+  it 'sends all mentors by search' do
+    query_string = <<-GRAPHQL
+      query {
+        mentors {
+          name
+          email
+        }
+      }
+    GRAPHQL
 
+    post '/graphql', params: { query: query_string }
+
+    result = JSON.parse(response.body)
+    expect(result["data"]["mentors"][0]["name"]).to eq('Mary')
+    expect(result["data"]["mentors"][1]["name"]).to eq('Kayla')
+    expect(result["data"]["mentors"][2]["name"]).to eq('Ben')
+  end
+  it 'sends all mentors by search with search params' do
+    variables = { "zip_code": "80202"}
+    query_string = <<-GRAPHQL
+      query($zip_code: String!) {
+        mentors($zip_code) {
+          name
+          email
+        }
+      }
+    GRAPHQL
+
+    post '/graphql', params: { query: query_string, variables: variables}
+
+    result = JSON.parse(response.body)
+    require "pry"; binding.pry
   end
 end
