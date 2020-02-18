f760a8fb384a42e5ed0014e9f2c7f0244a9b7a76
Mary Lang <mcl86@cornell.edu>
2020-02-18 12:15:57 -0700

    Add zip code and age to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_17_152914) do
+ActiveRecord::Schema.define(version: 2020_02_18_190001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 2020_02_17_152914) do
     t.text "advice_question"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "zip_code"
+    t.integer "age"
   end
 
 end
