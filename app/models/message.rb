6a16a5b4f1069d33e2fffcec291b035b8ae8e5e8
Christopher Kelling <cj.kelling88@gmail.com>
2020-02-20 09:48:14 -0700

    Add model validations for conversations and messages

---
+++ app/models/message.rb
@@ -0,0 +1,8 @@
+class Message < ApplicationRecord
+  belongs_to :conversation
+  belongs_to :user
+
+  validates_presence_of :body
+  validates_presence_of :conversation_id
+  validates_presence_of :user_id
+end
