d29d18290d0d04e2dc88cd78f8596edc74d4d64a
Christopher Kelling <cj.kelling88@gmail.com>
2020-02-20 09:49:36 -0700

    Update user model for a has_many relationship to conversations

--- app/models/mentor_profile.rb
+++ app/models/mentor_profile.rb
@@ -1,10 +1,10 @@
-class MentorProfile< ApplicationRecord
+class MentorProfile < ApplicationRecord
   belongs_to :user
 
-  validates_presence_of :field_of_knowledge 
-  validates_presence_of :experience_level 
-  validates_presence_of :work_day_question 
-  validates_presence_of :enjoyment_question 
-  validates_presence_of :teaching_points_question 
-  validates_presence_of :advice_question 
+  validates_presence_of :field_of_knowledge
+  validates_presence_of :experience_level
+  validates_presence_of :work_day_question
+  validates_presence_of :enjoyment_question
+  validates_presence_of :teaching_points_question
+  validates_presence_of :advice_question
 end
