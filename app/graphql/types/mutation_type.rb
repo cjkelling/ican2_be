676a5dc9f2fb047c3ac298bac301c3fb02d3d470
Christopher Kelling <51389815+cjkelling@users.noreply.github.com>
2020-02-20 08:39:55 -0700

    Update mutation_type.rb

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,8 @@ module Types
   class MutationType < Types::BaseObject
     field :create_user, mutation: Mutations::CreateUser
     field :create_connection, mutation: Mutations::CreateConnection
+    field :destroy_connection, mutation: Mutations::DestroyConnection	
+    field :destroy_user, mutation: Mutations::DestroyUser	
+    field :update_user, mutation: Mutations::UpdateUser	
   end
 end
