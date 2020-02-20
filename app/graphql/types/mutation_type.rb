ea3ab3d5a354691472cde88e4fd0725bbac6fbf0
Mary Lang <mcl86@cornell.edu>
2020-02-19 18:26:02 -0700

    Add destroy connection functionality and spec

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,8 @@ module Types
   class MutationType < Types::BaseObject
     # TODO: remove me
     field :create_user, mutation: Mutations::CreateUser
+
+
+    field :destroy_connection, mutation: Mutations::DestroyConnection
   end
 end
