a035922ad05b7e6944396190956b3d22981f7efd
Mary Lang <mcl86@cornell.edu>
2020-02-24 10:59:44 -0700

    Change to allow for conversation information

--- app/graphql/types/message_type.rb
+++ app/graphql/types/message_type.rb
@@ -1,9 +1,9 @@
 module Types
   class MessageType < Types::BaseObject
     field :id, Int, null: false
-    field :sender_id, Int, null: false
-    field :recipient_id, Int, null: false
+    field :user_id, Int, null: false
     field :body, String, null: false
+    field :conversation, Types::ConversationType, null: false
     field :read, Boolean, null: false
   end
 end
